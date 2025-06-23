f5f5dde27da7d4af0112afc916df6685be59dfcc
ssshiponu <sshiponuddin22@gmail.com>
2025-06-23 22:07:57 +0600

    easy setup process created

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+# Setup environment
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+source $(which virtualenvwrapper.sh)
+
 mkvirtualenv portfolio-venv --python=python3.12
 pip install -r requirements.txt
 python manage.py collectstatic
+
 echo "SECRET_KEY='$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')'" > .env
 echo "DEBUG='False'" >> .env
+
 read -p "username: " username
 cd portfolio
 echo "ALLOWED_HOSTS += ['${username}.pythonanywhere.com']" >> settings.py
