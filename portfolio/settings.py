7c312b38f0f55a86f4f42fce6e5d8971107e1413
ssshiponu <sshiponuddin22@gmail.com>
2025-06-23 12:47:04 +0600

    static setings update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -108,9 +108,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 
-# STATICFILES_DIRS = [ BASE_DIR / "static", ]
+STATICFILES_DIRS = [ BASE_DIR / "static", ]
 
 MEDIA_URL = 'media/'
 
