50d27e3f577b070b91bb5a6029eb6f8c9775485f
Lloyd Hangoma <hangoma30@gmail.com>
2025-09-06 09:29:48 +0200

    Add health check endpoint for Render

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # The health check path for Render
+    # Make sure your views.py file has a 'health_check' function!
+    path('status/', views.health_check, name='health_check'),
+
     path('', views.index, name='index'),
     path('robots.txt', views.robots, name='robots'),
     path('sitemap.xml', views.sitemap, name='sitemap'),
